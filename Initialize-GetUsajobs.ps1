7ce36cc9d4df3b72ae91b0769078c4d2732aab00
Derek Miller <derekjmiller@gmail.com>
2022-04-24 10:44:42 -0700

    add initial content to config

--- Initialize-GetUsajobs.ps1
+++ Initialize-GetUsajobs.ps1
@@ -2,7 +2,12 @@ $config = 'config/config.json'
 
 try{
     if(-not (Test-Path $config)){
-        New-Item -Path $config -Force
+        New-Item -Path $config -Force | Out-Null
+        Set-Content -ErrorAction Stop -Path $config -Value @"
+{
+    "server":"data.usajobs.gov"
+}
+"@
     }
 }
 catch{
