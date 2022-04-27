0742ca2b0bd1114f1df10202ead6811d3098eb06
Derek Miller <derekjmiller@gmail.com>
2022-04-27 14:46:06 -0700

    add publish date filter and combine results option

--- scripts/AllQueriesController.ps1
+++ scripts/AllQueriesController.ps1
@@ -1,15 +1,37 @@
-#Use this script to generate a report with a tab for each of your saved searches
+#Use this script to generate a report with a tab for each of your saved searches or combine multiple queries into one worksheet
 #Ex: AllQueriesController.ps1 -ApiKey $Apikey
 
 [cmdletbinding()]
 param(
     [string]$Path = "$((Get-Date).ToString("yyyy-MM-dd"))_USAJOBSAllSearches.xlsx",
     [string]$ApiKey = $Global:ApiKey,
-    [string[]]$ControlNumberFilter
+    [string[]]$ControlNumberFilter,
+    [int]$PostedDaysAgo,
+    [switch]$CombineResults
 )
 
-foreach($key in $SavedQueries.Keys){
-    $jobs = Find-Usajobs -ApiKey $ApiKey -SavedQuery $key -ControlNumberFilter $ControlNumberFilter -RemoveMultipleLocations -Verbose | Sort-Object -Property 'LowPay'
+$reqSplat = @{
+    ApiKey = $ApiKey
+}
+if($ControlNumberFilter){ $reqSplat['ControlNumberFilter'] = $ControlNumberFilter }
+if($PostedDaysAgo){ $reqSplat['PostedDaysAgo'] = $PostedDaysAgo }
 
-    Export-UsajobsReport -ReportObject $jobs -Name $key -Path $Path
+$combinedResults = @()
+
+$combinedResults
+foreach($key in $SavedQueries.Keys){
+    $jobs = Find-Usajobs @reqSplat -SavedQuery $key -RemoveMultipleLocations | Sort-Object -Property 'LowPay'
+
+    if($CombineResults){
+        $combinedResults += $jobs
+    }
+    else{
+        Export-UsajobsReport -ReportObject $jobs -Name $key -Path $Path
+    }
+}
+
+if($CombineResults){
+    $combinedResults = $combinedResults | Sort-Object -Property 'ControlNumber' -Unique
+
+    Export-UsajobsReport -ReportObject $combinedResults -Name 'Combined Queries' -Path $Path
 }
